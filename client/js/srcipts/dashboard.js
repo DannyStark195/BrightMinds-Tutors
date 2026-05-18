e0941b96703ca21478f1f7490e1b51d3fbe65f71
Danny Stark <1ironman19101@gamil.com>
2026-05-18 22:48:34 +0100

    working on dashboard

---
+++ client/js/srcipts/dashboard.js
@@ -0,0 +1,13 @@
+const numPayments = document.querySelector('#no-payments');
+const numBookings = document.querySelector('#no-bookings');
+const numReviews = document.querySelector('#no-reviews');
+
+numBookings.textContent = '0';
+numPayments.textContent = '0';
+numReviews.textContent = '0';
+
+const nextLessonDescription = document.querySelector('#next-lesson-desc');
+const nextLessonDate = document.querySelector('#next-lesson-date');
+const paymentStatusDescription = document.querySelector('#payment-status-desc');
+const paymentStatusBalance = document.querySelector('#payment-status-balance');
+
