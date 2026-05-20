ade767dd267efbe9ef0882f08a91fb45aa94ef12
Danny Stark <1ironman19101@gamil.com>
2026-05-20 23:29:48 +0100

    ratings form done

--- client/js/srcipts/review.js
+++ client/js/srcipts/review.js
@@ -1,5 +1,15 @@
 const reviewForm = document.querySelector('.review-form');
 const ratings = document.querySelectorAll('.rating-btn');
+const bookings = document.querySelector('#booking');
+
+console.log(bookings);
+
+bookings.innerHTML = `
+        <option>Mathematics with Miss Adaeze</option>
+        <option>Biology with Mr Tunde</option>
+        <option>English with Miss Ngozi</option>
+`
+
 console.log(ratings);
 let currentRating = 0;
 ratings.forEach(ratingBtn =>{
@@ -7,13 +17,12 @@ ratings.forEach(ratingBtn =>{
                 const rating = Number(ratingBtn.dataset.star);
                 console.log(rating);
                 currentRating = rating;
-                
                 ratings.forEach(ratingBtn =>{
                         const rating = Number(ratingBtn.dataset.star);
                         const ratingIcon = ratingBtn.previousElementSibling;
                         ratingIcon.classList.remove('fa-solid');
                         if(rating <= currentRating){
-                                ratingIcon.classList.toggle('fa-solid');
+                                ratingIcon.classList.add('fa-solid');
                         }
                 })
         })
