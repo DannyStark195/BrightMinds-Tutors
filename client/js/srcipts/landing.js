c34c258668c61517a2cf182237bfb8cdb2e0d1f1
Danny Stark <1ironman19101@gamil.com>
2026-05-21 14:20:33 +0100

    fixed file url links

--- client/js/srcipts/landing.js
+++ client/js/srcipts/landing.js
@@ -1,12 +1,12 @@
 import { openForm } from "../auth/authForm.js";
 const overlay = document.querySelector('.dark-overlay');
-const loginForm = overlay?.querySelector('.login-form-container');
+const loginFormContainer = overlay?.querySelector('.login-form-container');
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
     const params = new URLSearchParams(window.location.search);
     if(params.get('auth') === 'required'){
-        openForm(overlay, loginForm);
+        openForm(overlay, loginFormContainer);
     }
 
 });
