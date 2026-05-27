d24b1bcd03f40d6f08c5dd23a6b9a80ce69910b1
Danny Stark <1ironman19101@gamil.com>
2026-05-27 16:19:00 +0100

    still working on booking flow

--- client/js/scripts/book.js
+++ client/js/scripts/book.js
@@ -4,18 +4,20 @@ const bookingForm = document.querySelector('.booking-form');
 const bookingProgress = document.querySelector('.booking-progress');
 const backBtn = bookingForm.querySelector('.back-btn');
 const continueBtn = bookingForm.querySelector('.continue-btn');
-let selctedSubject = null;
+let selectedSubject = null;
 let selectedLevel = null;
 let selectedTime = null;
 let selectedHr = null;
-let selctedDay = []
+let selectedDays = []
 let selectedTimeWindow = null;  
 let selectedLocation = null;
+let selectedStartDate = null;
 
 const timeWindowsByHr = {
             "2hrs": ['9.am - 11.am', '12.pm - 2.pm', '4.pm - 6.pm'],
             "4hrs": ['9.am - 12.pm', '12.pm - 3.pm', '3.pm - 6.pm']
 }
+
 let currentStep = 1;
 
 function updateStepDisplay(currentStep) {
@@ -32,7 +34,9 @@ function updateStepDisplay(currentStep) {
             activateElement(step);
         }
     });
+    console.log('updatstep');
 }
+
 function validateStep(currentStep){
    if(currentStep === 1){
         return validateStep1()
@@ -49,6 +53,8 @@ function validateStep(currentStep){
 }
 
 function setBookingFlow(){
+    console.log('booking-flow');
+    
     const step1 = bookingForm.querySelector('.step-1');
     const bookSubjects = step1.querySelector('.book-subjects');
     const subjectOptions = bookSubjects.querySelectorAll('.sub');
@@ -57,27 +63,29 @@ function setBookingFlow(){
         option.addEventListener('click', ()=>{
             subjectOptions.forEach(option => deactivateElement(option));
             activateElement(option);
+
+            const subjectBtn = option.querySelector('.subject-btn');
+            console.log(subjectBtn);
+            
+                if(subjectBtn.checked){
+                    selectedSubject = subjectBtn.value;
+                }
+                console.log(subjectBtn.checked)
         });
     });
-    const subjectBtns = bookSubjects.querySelectorAll('.subject-btn');
-    subjectBtns.forEach(subjectBtn =>{
-        if(subjectBtn.checked){
-            selctedSubject = subjectBtn.value;
-        }
-    });
+   
     const gradeLevels = step1.querySelector('.levels');
     const gradeLevelOptions = gradeLevels.querySelectorAll('.level');
     gradeLevelOptions.forEach(option =>{
         option.addEventListener('click', ()=>{
             gradeLevelOptions.forEach(option => deactivateElement(option));
             activateElement(option);
+
+            const gradeLevelBtn = option.querySelector('.grade-btn');
+                if(gradeLevelBtn.checked){
+                    selectedLevel = gradeLevelBtn.value;
+                }
         });
-    });
-    const gradeLevelBtns = gradeLevels.querySelectorAll('.grade-level-btn');
-    gradeLevelBtns.forEach(gradeLevelBtn =>{
-        if(gradeLevelBtn.checked){
-            selectedLevel = gradeLevelBtn.value;
-        }
     });
 
     const times = step1.querySelector('.times');
@@ -86,15 +94,14 @@ function setBookingFlow(){
         option.addEventListener('click', ()=>{
             timeOptions.forEach(option => deactivateElement(option));
             activateElement(option);
+
+            const timeBtn = option.querySelector('.times-btn');
+                if(timeBtn.checked){
+                    selectedTime = timeBtn.value;
+                }
         });
-    } 
-    );
-    const timeBtns = times.querySelectorAll('.time-btn');
-    timeBtns.forEach(timeBtn =>{
-        if(timeBtn.checked){
-            selectedTime = timeBtn.value;
-        }
     });
+    
 
     const hrs = step1.querySelector('.hrs');
     const hrOptions = hrs.querySelectorAll('.hr');
@@ -102,30 +109,28 @@ function setBookingFlow(){
         option.addEventListener('click', ()=>{
             hrOptions.forEach(option => deactivateElement(option));
             activateElement(option);
+            const hrBtn = option.querySelector('.hrs-btn');
+                if(hrBtn.checked){
+                    selectedHr = hrBtn.value;
+                }
         });
     });
-    const hrBtns = hrs.querySelectorAll('.hr-btn');
-    hrBtns.forEach(hrBtn =>{
-        if(hrBtn.checked){
-            selctedHr = hrBtn.value;
-        }
-    });
+
     const step2 = bookingForm.querySelector('.step-2');
     const dayOptions = step2.querySelectorAll('.day');
     dayOptions.forEach(option =>{
         option.addEventListener('click', ()=>{
             option.classList.toggle('active');
             const dayValue = option.querySelector('.days-btn').value;
-            if(selctedDay.includes(dayValue)){
-                selctedDay = selctedDay.filter(day => day !== dayValue);
+            if(selectedDays.includes(dayValue)){
+                selectedDays = selectedDays.filter(day => day !== day);
             }else{
-                selctedDay.push(dayValue);
+                selectedDays.push(dayValue);
             }
         });
     });
 
     const timeWindows = step2.querySelector('.time-windows');
-
     timeWindows.innerHTML =`
        <label class="option-card window">
             ${selectedHr === '2hrs'? timeWindowsByHr["2hrs"][0]: timeWindowsByHr["4hrs"][0]}
@@ -133,7 +138,7 @@ function setBookingFlow(){
         </label>
         <label class="option-card window">
             ${selectedHr === '2hrs'? timeWindowsByHr["2hrs"][1]: timeWindowsByHr["4hrs"][1]}
-            <input type="radio" name="timeWindow" value="1${selectedHr === '2hrs'? timeWindowsByHr["2hrs"][1]: timeWindowsByHr["4hrs"][1]} class="hidden-radio window-btn">
+            <input type="radio" name="timeWindow" value="${selectedHr === '2hrs'? timeWindowsByHr["2hrs"][1]: timeWindowsByHr["4hrs"][1]}" class="hidden-radio window-btn">
         </label>
         <label class="option-card window active">
             ${selectedHr === '2hrs'? timeWindowsByHr["2hrs"][2]: timeWindowsByHr["4hrs"][2]}
@@ -145,14 +150,13 @@ function setBookingFlow(){
         option.addEventListener('click', ()=>{
             windowOptions.forEach(option => deactivateElement(option));
             activateElement(option);
+            const windowBtn = option.querySelector('.window-btn');
+            if(windowBtn.checked){
+                selectedTimeWindow = windowBtn.value;
+            }
         });
-    const windowBtns = timeWindows.querySelectorAll('.window-btn');
-    windowBtns.forEach(windowBtn =>{
-        if(windowBtn.checked){
-            selectedTimeWindow = windowBtn.value;
-        }
     });
-    });
+    
     const startDate = step2.querySelector('.start-date');
     startDate.addEventListener('change', ()=>{
         selectedStartDate = startDate.value;
@@ -184,14 +188,17 @@ function setBookingFlow(){
 }
 
 function validateStep1(){
-    if(!selctedSubject || !selectedLevel || !selectedTime || !selctedHr){
+    console.log(selectedSubject, selectedLevel, selectedTime, selectedHr);
+    
+    if(!selectedSubject || !selectedLevel || !selectedTime || !selectedHr){
         // alert('Please select a subject, grade level, time and hours per session to continue');
         return false;
     }
     return true;
 }
 function validateStep2(){
-    if(selctedDay.length === 0 || !selectedTimeWindow || !selectedStartDate){
+    console.log(selectedDays, selectedTimeWindow, selectedStartDate)
+    if(selectedDays.length === 0 || !selectedTimeWindow || !selectedStartDate){
         return false;
     }
     return true;
@@ -205,21 +212,22 @@ function validateStep3(){
 function validateStep4(){
     return true;
 }
+
 setBookingFlow();
- continueBtn.addEventListener('click', () => {
+updateStepDisplay(1);
+continueBtn.addEventListener('click', () => {
        const valid = validateStep(currentStep);
-        if(valid){
-            currentStep+=1
-            updateStepDisplay(currentStep);
+        if(!valid){
+            return
         }
+        currentStep+=1
+        updateStepDisplay(currentStep);
     });
 
 backBtn.addEventListener('click', () => {
         currentStep-=1
         updateStepDisplay(currentStep);
     });
-
-updateStepDisplay(1);
 
 bookingForm.addEventListener('submit', (e) => {
     e.preventDefault();
