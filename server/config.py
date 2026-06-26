1c6de7566ad4bb800e30258fe2144e71ad3a3a84
Danny Stark <1ironman19101@gamil.com>
2026-06-26 21:13:05 +0100

    fixing a production bug

--- server/config.py
+++ server/config.py
@@ -13,9 +13,9 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=1)
 
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
-    # MAIL_USE_TLS = False
+    MAIL_PORT = 587
+    MAIL_USE_SSL = False
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
     MAIL_DEFAULT_SENDER = os.environ.get('EMAIL_USER')
