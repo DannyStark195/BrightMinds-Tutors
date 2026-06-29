8525586656d51f63cdcaf46f54e022d9e0f00855
Danny Stark <1ironman19101@gamil.com>
2026-06-29 20:11:32 +0100

    still working of oauth with google 3

--- server/app/auth/auth.py
+++ server/app/auth/auth.py
@@ -14,7 +14,7 @@ google_bp = make_google_blueprint(
     client_id=os.environ.get('GOOGLE_CLIENT_ID'),
     client_secret=os.environ.get('GOOGLE_CLIENT_SECRET'),
     scope=["openid", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"],
-    # redirect_url="/api/auth/google/callback"
+    redirect_to="auth.google_callback"
 )
 
 auth = Blueprint('auth',__name__)
